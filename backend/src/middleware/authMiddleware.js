76df25675db9dcc791e9b78bcc84ea9066a049fe
hussainaabid99 <aabid.hu99@gmail.com>
2025-05-01 00:11:08 +0530

    Added auth middleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,48 @@
+import { StatusCodes } from "http-status-codes";
+import { CustomErrorResponse } from "../common/responseObject.js";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/serverConfig.js";
+import userRepsitory from "../repositories/userRepository.js";
+
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.headers["x-access-token"];
+    if (!token)
+      return res.status(StatusCodes.UNAUTHORIZED).json(
+        CustomErrorResponse({
+          message: "No auth token provided",
+        })
+      );
+
+    const response = jwt.verify(token, JWT_SECRET);
+
+    if (!response)
+      return res.status(StatusCodes.FORBIDDEN).json(
+        CustomErrorResponse({
+          explanation: "Invalid data sent from the client",
+          message: "Invalid auth token provided",
+        })
+      );
+
+    const user = await userRepsitory.getById(response.id);
+    req.user = user.id;
+
+    next();
+  } catch (error) {
+    console.log("Auth middleware error", error);
+    if (
+      error.name === "JsonWebTokenError" ||
+      error.name === "TokenExpiredError"
+    ) {
+      return res.status(StatusCodes.FORBIDDEN).json(
+        CustomErrorResponse({
+          explanation: "Invalid data sent from the client",
+          message: "Invalid auth token provided",
+        })
+      );
+    }
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(error));
+  }
+};
