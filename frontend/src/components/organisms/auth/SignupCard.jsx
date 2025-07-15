77f93c1bd2308b0ca43fbdaab6177eddd2568e73
hussainaabid99 <aabid.hu99@gmail.com>
2025-07-16 01:02:19 +0530

    Added signup card for auth page

---
+++ frontend/src/components/organisms/auth/SignupCard.jsx
@@ -0,0 +1,88 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { useState } from "react";
+
+export const SignupCard = () => {
+  const [signupForm, setSignupForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    username: "",
+  });
+  return (
+    <Card className="w-full h-full">
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>Signup to access your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="space-y-3">
+          <Input
+            placeholder="Email"
+            required
+            onChange={(e) =>
+              setSignupForm({ ...signupForm, email: e.target.value })
+            }
+            value={signupForm.email}
+            type="email"
+            disabled={false}
+          />
+          <Input
+            placeholder="Password"
+            required
+            onChange={(e) =>
+              setSignupForm({ ...signupForm, password: e.target.value })
+            }
+            value={signupForm.password}
+            type="password"
+            disabled={false}
+          />
+          <Input
+            placeholder="Confirm Password"
+            required
+            onChange={(e) =>
+              setSignupForm({
+                ...signupForm,
+                confirmPassword: e.target.value,
+              })
+            }
+            value={signupForm.confirmPassword}
+            type="password"
+            disabled={false}
+          />
+          <Input
+            placeholder="Your Username"
+            required
+            onChange={(e) =>
+              setSignupForm({
+                ...signupForm,
+                username: e.target.value,
+              })
+            }
+            value={signupForm.username}
+            type="text"
+            disabled={false}
+          />
+          <Button disabled={false} size="lg" type="Submit" className="w-full">
+            Continue
+          </Button>
+        </form>
+        <Separator className="my-5"></Separator>
+        <p className="text-sm text-muted-foreground mt-4">
+          Already have an account ?{" "}
+          <span className="text-sky-600 hover:underline cursor-pointer">
+            SignIn
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ frontend/src/pages/auth/Auth.jsx
@@ -0,0 +1,11 @@
+import { SignupCard } from "@/components/organisms/auth/SignupCard";
+
+export const Auth = () => {
+  return (
+    <div className="h-[100vh] flex items-center justify-center bg-theme">
+      <div className="md:h-auto md:w-[420px]">
+        <SignupCard />
+      </div>
+    </div>
+  );
+};
